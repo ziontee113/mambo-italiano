d167c6ef4475490e71b7c32b1aafb67129b138e9
ziontee113 <ziontee113@gmail.com>
2023-04-06 22:13:06 +0700

    Simple Background Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode)
-  }
-
   return (
-    <div
-      className={`h-screen flex flex-col justify-center items-center ${
-        isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-100'
-      }`}
-    >
-      <button className="absolute top-0 right-0 m-4" onClick={toggleDarkMode}>
-        <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
-      </button>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <p className="text-lg mt-4">Welcome to our website!</p>
+    <div>
+      <div className="flex h-screen w-screen justify-center items-center">
+        <div className="-z-10 absolute w-full h-full">
+          <Image
+            src="/images/00029.jpg"
+            alt="Background Image"
+            style={{ objectFit: 'cover' }}
+            fill
+          />
+        </div>
+        <div className="">
+          <h1 className="text-8xl font-bold text-white">Hello World</h1>
+        </div>
+      </div>
     </div>
   )
 }
